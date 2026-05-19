a189b1da341522e055d2d97b303172ba45800c3b
sgh <esghaier.ainformation@gmail.com>
2026-05-19 19:10:48 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,7 @@
 const CACHE_NAME = "admin-cache-v1";
+
 const urlsToCache = [
+  "./",
   "./index.html",
   "./manifest.json",
   "./styles.css",
@@ -8,16 +10,4 @@ const urlsToCache = [
   "https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"
 ];
 
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
-});
-
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
-});
+//
